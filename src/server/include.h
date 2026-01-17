c4123db31e2566c468985f747c8da76f55c16966
czhang076 <czhang076@e.ntu.edu.sg>
2026-01-17 19:00:32 +0800

    1

--- src/server/include.h
+++
@@ -1,4 +0,0 @@
-/* Copyright (c) 2026, Yaozeran, Zhangchenzhi, Zhangsenyao
- */
-
-#include "compat.h"
--- src/server/main.cc
+++
@@ -1,8 +0,0 @@
-/* Copyright (c) 2026, Yaozeran, Zhangchenzhi, Zhangsenyao
- */
-
-#include "include.h"
-
-int main(int argc, char* argv[]) {
-  return 0;
-}
